336da489c1fc2c11dcd4d80d059fe1f24c9cb5d9
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2023-10-06 17:59:18 +0200

    1

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,25 +1,4 @@
-import * as RA from '@effect/data/ReadonlyArray';
-
-export const kebabize = (str: string) =>
-	str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? '-' : '') + $.toLowerCase());
-
-// RegExp utilities
-const zeroOrMore = (s: string): string => `(?:${s})*`;
-const oneOrMore = (s: string): string => `(?:${s})+`;
-const repeatBetween = (s: string, low: string, high = ''): string => `(?:${s}){${low},${high}}`;
-const optional = (s: string): string => `(?:${s})?`;
-const either = (...args: ReadonlyArray<string>): string => `(?:${RA.join(args, '|')})`;
-const makeLine = (s: string): string => `^${s}$`;
-
-const star = /\*/.source;
-const dot = /\./.source;
-const number = /\d+/.source;
-const letter = /[A-Za-z]/.source;
-const slash = /\//.source;
-const colon = /:/.source;
-const singleQuote = /'/.source;
-
-// Constants defined according to https://w3c.github.io/webappsec-csp/#grammardef-scheme-part
+// Patterns defined according to https://w3c.github.io/webappsec-csp/#grammardef-scheme-part
 const schemeFirstChar = letter;
 const schemeNextChar = /[A-Za-z0-9+.-]/.source;
 const schemePart = `${schemeFirstChar}${zeroOrMore(schemeNextChar)}`;
@@ -42,7 +21,7 @@ const hostSource = makeLine(
 	optional(schemePart + schemeSep) + hostPart + optional(colon + portPart) + optional(pathPart)
 );
 
-const base64ValueChar = /[A-Za-z0-9+\/-_]/.source;
+const base64ValueChar = /[A-Za-z0-9+/-_]/.source;
 const base64Value = oneOrMore(base64ValueChar) + repeatBetween('=', '0', '2');
 const nonceBase = /nonce-/.source;
 const nonceSource = `${singleQuote}${nonceBase}${base64Value}${singleQuote}`;
